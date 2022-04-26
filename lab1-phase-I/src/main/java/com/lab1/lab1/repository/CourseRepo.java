86133e0bdbf92b56a954a2f4f5ecae72617553c1
Meresa <meresa27@gmail.com>
2022-04-26 14:56:13 -0500

    course crud operations added

--- lab1-phase-I/src/main/java/com/lab1/lab1/repository/CourseRepo.java
+++ lab1-phase-I/src/main/java/com/lab1/lab1/repository/CourseRepo.java
@@ -21,4 +21,23 @@ public class CourseRepo {
     public List<Course> getAll() {
         return courses;
     }
+
+    public void delete(int id) {
+        courses.stream().filter(c -> c.getId() == id).forEach( c -> courses.remove(c));
+    }
+    public Course getById(int id) {
+        return courses.stream()
+                .filter( c -> c.getId()==id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public void save(Course c) {
+        var checkIfCourseExists = courses
+                .stream()
+                .noneMatch(l -> l.getId() == c.getId());
+        if(checkIfCourseExists){
+            courses.add(c);
+        }
+    }
 }
