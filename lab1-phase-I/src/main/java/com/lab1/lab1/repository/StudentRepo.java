6fe87f4751300d340da2bfc003816fb0969342cc
Meresa <meresa27@gmail.com>
2022-04-26 14:47:49 -0500

    student crud operations added

--- lab1-phase-I/src/main/java/com/lab1/lab1/repository/StudentRepo.java
+++ lab1-phase-I/src/main/java/com/lab1/lab1/repository/StudentRepo.java
@@ -1,17 +1,53 @@
 package com.lab1.lab1.repository;
 
+import com.lab1.lab1.model.Course;
 import com.lab1.lab1.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class StudentRepo {
     private static List<Student> students;
+    @Autowired
+    private static CourseRepo courseRepo;
 
     static {
         students =  new ArrayList<>();
+        var s = new Student();
+        s.setId(1);
+        s.setFirstName("Meresa");
+        s.setLastName("Gebrewahd");
+        s.setEmail("meresa27@gmail.com");
+        s.setMajor("Compro");
+        s.setCoursesTaken(courseRepo.getAll());
+    }
 
+    public void save(Student s) {
+        var checkIfExists = students
+                .stream()
+                .noneMatch(l -> l.getId()==s.getId());
+        if(checkIfExists) {
+            students.add(s);
+        }
+    }
+
+    public void delete(int id) {
+        students.stream()
+                .filter( l-> l.getId() == id)
+                .forEach( l ->students.remove(l));
+    }
+
+    public Student getById(int id) {
+        return students.stream()
+                .filter( l -> l.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+    public List<Student> getAll() {
+        return students;
     }
 }
