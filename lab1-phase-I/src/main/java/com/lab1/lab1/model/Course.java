2ae4fb7c15810ac639bde2e8f18731485023a3c8
Meresa <meresa27@gmail.com>
2022-04-26 14:18:01 -0500

    course model added

---
+++ lab1-phase-I/src/main/java/com/lab1/lab1/model/Course.java
@@ -0,0 +1,18 @@
+package com.lab1.lab1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private int id;
+    private String name;
+    private String code;
+
+}
