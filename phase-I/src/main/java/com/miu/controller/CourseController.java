03b67e85a61723456552e7e6281a87594c360127
Shree Krishna <shreekrishna905@gmail.com>
2022-04-26 18:35:49 -0500

    creates inital url versioning

--- phase-I/src/main/java/com/miu/controller/CourseController.java
+++ phase-I/src/main/java/com/miu/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/v1/courses")
 public class CourseController {
 
     private CourseService courseService;
--- phase-I/src/main/java/com/miu/controller/StudentController.java
+++ phase-I/src/main/java/com/miu/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api/v1/students")
 public class StudentController {
 
     StudentService studentService;
